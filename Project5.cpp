14ce1ef9de3c3866249ddcec5c9591e1a1671092
Liam Tolkkinen <liamtolkki@gmail.com>
2023-03-29 21:31:54 -0400

    Added file reader, adds all words to vector

--- Project5.cpp
+++ Project5.cpp
@@ -5,8 +5,30 @@ Data Structures and Algorithms
 3/29/2023
 */
 #include "Trie.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+using namespace std;
+
 int main()
 {
+    ifstream fileIn("wordlist_windows.txt"); //gets the word file
+    vector<string> wordBank;
+    cout << "Opening file..." << endl;
+    //make sure there is no error:
+    if (fileIn.good()) {
+        string word;
+        cout << "File opened sucessfully!" << endl;
+        while (!fileIn.eof()) {
+            fileIn >> word;
+            wordBank.push_back(word);
+        }
+    } else {
+        cerr << "Couldn't open file!" << endl;
+    }
+
+
 
     return 0;
 }
