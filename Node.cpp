96c40d7fe55593b710d28fc18fcd7efd1294a2ee
Liam Tolkkinen <liamtolkki@gmail.com>
2023-04-02 01:59:10 -0400

    Added constuctors and a charArray

---
+++ Node.cpp
@@ -0,0 +1,10 @@
+#include "Node.h"
+
+Node::Node() // default node constructor
+{
+    for (Node *charNode : charList)
+    { // upon creation, all branches will be
+        // initialized to null pointers
+        charNode = nullptr;
+    }
+}
--- Node.h
+++ Node.h
@@ -1,1 +1,11 @@
 #pragma once
+
+class Node
+{
+public:
+
+    Node();
+
+private:
+    Node *charList[27]; // will hold the pointers for the next char A-Z
+};
