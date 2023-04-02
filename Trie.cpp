9332b93719cf0ac5278baef7b8b7fb11c8bb9761
Liam Tolkkinen <liamtolkki@gmail.com>
2023-04-02 01:20:35 -0400

    Started adding the body of both files

---
+++ Trie.cpp
@@ -0,0 +1,15 @@
+using namespace std;
+#include "Trie.h"
+
+bool Trie::insert(string str)
+{ // inserts a string
+}
+
+int Trie::count()
+{
+    return count;
+}
+int Trie::getSize()
+{
+    return size;
+}
--- Trie.h
+++ Trie.h
@@ -1,1 +1,17 @@
 #pragma once
+
+#include "Node.h"
+#include <string>
+using namespace std;
+
+class Trie
+{
+public:
+    Trie(); // default constructor
+    int count();
+    int getSize();
+
+private:
+    int wordCount, size; // int vars
+    bool insert(string str);
+};
