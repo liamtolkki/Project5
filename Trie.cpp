cc12982e507ecfa62a62346270415eee2515ef35
Liam Tolkkinen <liamtolkki@gmail.com>
2023-04-03 16:58:47 -0400

    Added constructor code to initialize the trie

--- Trie.cpp
+++ Trie.cpp
@@ -1,6 +1,11 @@
 using namespace std;
 #include "Trie.h"
 
+Trie::Trie()
+{ // make a new trie with a root node (Empty)
+    root = new Node();
+}
+
 bool Trie::insert(string str)
 { // inserts a string
 }
