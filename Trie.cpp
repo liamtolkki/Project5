f7e7eec4a17c35c6d17fe47f39d06b39bcb1f6bf
Liam Tolkkinen <liamtolkki@gmail.com>
2023-04-06 14:35:39 -0400

    This will need fixed

--- Trie.cpp
+++ Trie.cpp
@@ -3,11 +3,21 @@ using namespace std;
 
 Trie::Trie()
 { // make a new trie with a root node (Empty)
-    root = new Node()
+    Node *root = new Node();
 }
 
 bool Trie::insert(string str)
-{ // inserts a string
+{                         // inserts a string
+    Node *current = root; // holds the starting node
+    for (int i = 0; i < str.size(); i++)
+    {                                   // this repeats for every character in the string
+        char current = str[i];          // gets the current character in the string
+        int arrIndex = ('a' - current); // used for progressing the trie node index
+        if (current->getNext(arrIndex) != nullptr)
+        {
+            current = current->getNext(arrIndex);
+        }
+    }
 }
 
 int Trie::getCount()
